18d125dc204e5b09afb374baec13c387b76dff7f
Camillo Di Marco <camus1946@gmail.com>
2017-03-16 18:12:05 +0100

    Update register.php

--- register.php
+++ register.php
@@ -1,8 +1,8 @@
 <?php
 
 // PARAMETRI DA MODIFICARE
-$WEBHOOK_URL = 'https://{APP NAME}.herokuapp.com/execute.php';
-$BOT_TOKEN = '{TOKEN}';
+$WEBHOOK_URL = 'https://rosetonews.herokuapp.com/execute.php';
+$BOT_TOKEN = '30172895:AAEadXydreyJPX-fzqrtP5ku_FcPAV85pE';
 
 // NON APPORTARE MODIFICHE NEL CODICE SEGUENTE
 $API_URL = 'https://api.telegram.org/bot' . $BOT_TOKEN .'/';
