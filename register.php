931cde03aed5fb8aa2a1f461dbb0a1f95f796431
Camillo Di Marco <camus1946@gmail.com>
2017-03-16 18:14:40 +0100

    Update register.php

--- register.php
+++ register.php
@@ -1,7 +1,7 @@
 <?php
 
 // PARAMETRI DA MODIFICARE
-$WEBHOOK_URL = 'https://rosetonews.herokuapp.com/execute.php';
+$WEBHOOK_URL = https://rosetonews.herokuapp.com/execute.php;
 $BOT_TOKEN = '30172895:AAEadXydreyJPX-fzqrtP5ku_FcPAV85pE';
 
 // NON APPORTARE MODIFICHE NEL CODICE SEGUENTE
